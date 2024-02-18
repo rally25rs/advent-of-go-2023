7a566891a038f0199e0d87aaa39813599ad67979
Jeff Valore <rally25rs@yahoo.com>
2024-02-18 16:01:38 -0500

    day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -51,6 +51,42 @@ func readPart1Input(filename string) ([]race, error) {
 	return races, nil
 }
 
+func readPart2Input(filename string) (race, error) {
+	digitsRegex := regexp.MustCompile(`[^\d]+`)
+	inputFile, err := os.Open(filename)
+	if err != nil {
+		return race{}, err
+	}
+
+	lineSplitter := bufio.NewScanner(inputFile)
+	lineSplitter.Scan()
+	if lineSplitter.Err() != nil {
+		return race{}, err
+	}
+	timesLine := lineSplitter.Text()
+	lineSplitter.Scan()
+	if lineSplitter.Err() != nil {
+		return race{}, err
+	}
+	distancesLine := lineSplitter.Text()
+
+	raceTime := digitsRegex.ReplaceAllString(timesLine, "")
+	raceDistance := digitsRegex.ReplaceAllString(distancesLine, "")
+	time, err := strconv.Atoi(raceTime)
+	if err != nil {
+		return race{}, err
+	}
+	distance, err := strconv.Atoi(raceDistance)
+	if err != nil {
+		return race{}, err
+	}
+
+	return race{
+		time:     time,
+		distance: distance,
+	}, nil
+}
+
 func getRaceOutcomes(race race) (int, error) {
 	for speed := 0; speed < race.time; speed++ {
 		timeToMove := race.time - speed
@@ -81,6 +117,21 @@ func part1(filename string) int {
 	return winValue
 }
 
+func part2(filename string) int {
+	race, err := readPart2Input(filename)
+	if err != nil {
+		log.Fatal("Error reading input:", err)
+		os.Exit(1)
+	}
+	outcomes, err := getRaceOutcomes(race)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	return outcomes
+}
+
 func Execute(filename string) {
 	fmt.Println("Part 1:", part1(filename)) // sampleinput = 288 // input = 2269432
+	fmt.Println("Part 2:", part2(filename)) // sampleinput = 71503 // input = 35865985
 }
